08af26a2f0343d891fc83ff7df39b03fbcfdb874
Grzegorz Antoniak <ga@anadoxin.org>
2020-04-06 08:35:15 +0200

    array manipulation update

--- array_manipulation/python/array_manipulation.py
+++ array_manipulation/python/array_manipulation.py
@@ -2,16 +2,14 @@ import pprint as p
 
 def arrayManipulation(m, n, queries):
     arr = [0] * m
+    mval = 0
 
     for query in queries:
         if len(query) > 0:
             beg, end, val = query
             for i in range(beg, beg + (end - beg) + 1):
                 arr[i - 1] += val
-
-    mval = 0
-    for n in arr:
-        mval = max(mval, n)
+                mval = max(mval, arr[i - 1])
 
     return mval
 
@@ -24,13 +22,15 @@ def fromFile(name):
         arr.append([arr_size, op_count])
 
         for line in range(0, op_count):
-            line = f.readline().strip().split(' ')
+            line = f.readline().strip()
+            if len(line) == 0 or line[0] == '#': next
+            line = line.split(' ')
             beg, end, val = map(lambda x: int(x.strip()), line)
             op_arr.append([beg, end, val])
 
     arr.append(op_arr)
     return arr
 
-arr = fromFile("../1.txt")
+arr = fromFile("../15.txt")
 maxvalue = arrayManipulation(arr[0][0], arr[0][1], arr[1])
 print(maxvalue)
